521254514972dff21d9980d496acba09f7f645fb
Manik-Arora <manikarora2605@gmail.com>
2023-12-17 03:00:19 +0530

    Adding week2.2 solutions

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,32 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.get("/files", (req, res) => {
+  fs.readdir("./files/", "utf-8", (err, files) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to retrieve files" });
+    }
+    res.status(200).json(files);
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filepath = "./files/" + req.params.filename;
+  fs.readFile(filepath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(404).send("File not found");
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -39,11 +39,67 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  module.exports = app;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const todo = todos.find((t) => {
+    return t.id === parseInt(req.params.id);
+  });
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.status(200).json(todo);
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 100000) + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => {
+    return t.id === parseInt(req.params.id);
+  });
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.status(200).json(todos[todoIndex]);
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => {
+    return t.id === parseInt(req.params.id);
+  });
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+module.exports = app;
